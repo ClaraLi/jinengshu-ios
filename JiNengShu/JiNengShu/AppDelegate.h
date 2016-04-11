2c78e018ca41d956460baa54f82518e213d3462a
Lincon Zhang <zhangyilin@letv.com>
2016-04-11 12:29:48 +0800

    初始化工程

---
+++ JiNengShu/JiNengShu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JiNengShu
+//
+//  Created by zhang on 16/4/11.
+//  Copyright © 2016年 zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JiNengShu/JiNengShu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JiNengShu
+//
+//  Created by zhang on 16/4/11.
+//  Copyright © 2016年 zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
